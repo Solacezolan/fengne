3c0ef007b96ee2d947e8256532c52ed0e386907c
CN-20161124VJNH\Administrator <yzc769209808@sina.com>
2018-06-21 16:04:13 +0800

    首页修改

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -56,6 +56,18 @@
 		    </div>
 		  </div>
 	  </div>
+  	<div class="exclusive">
+   		<div gutter>
+    		<div class="exclusiveLeft" >
+    			<i class="iconfont icon-tuijian"></i>
+    		</div>
+    		<div  class="exclusiveRight">
+    			<i class="iconfont icon-xiangyou"></i>
+    			<span>登录</span> 
+    			 查看专属推荐
+    		</div>
+  		</div>
+   	</div>
   </div>
 </template>
 
@@ -96,16 +108,61 @@ export default {
 			height: 0.8rem;
 			div{
 				height: 0.4rem;
-				margin: 0.1rem 0;
+				margin: 0.1rem 0 0.05rem 0;
 				vertical-align: middle;
 				.iconfont{
-					font-size: 0.4rem;
+					font-size: 0.36rem;
 				}
-				p{
-					font-size: 0.14rem
-				}
+				
+			}
+			p{
+					font-size: 0.12rem
 			}
 		}
 	}
 }
+.exclusive{
+	border-bottom: 1px solid #ddd;
+	float: left;
+	width: 100%;
+	.exclusiveLeft{
+		float: left;
+		width: 10%;
+		text-align: center;
+		height: 0.35rem;
+		line-height: 0.35rem;
+		i{
+			color: #FCAA00;
+			font-size: 0.25rem;
+		}
+	}
+	.exclusiveRight{
+		float: left;
+		font-size: 0.12rem;
+		width: 90%;
+		box-sizing: border-box;
+		
+		i{
+			position: absolute;
+			color: #ddd;
+			right: 0.1333rem;
+			top:0;
+			
+		}
+		span{
+			color: #fcaa00;
+		}
+		padding-left:0.1rem;
+		position:relative;
+		text-align: left;
+		height: 0.35rem;
+		line-height: 0.35rem;
+		
+		cursor:pointer;
+		span{
+			font-weight: bold;
+		}
+	}
+}
+
 </style>
