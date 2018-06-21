03752317de125acf25a2adab78063c5901d0090c
CN-20161124VJNH\Administrator <yzc769209808@sina.com>
2018-06-21 11:37:35 +0800

    vue

---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template lang="html">
+  <div>
+     	<mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">  
+          <router-link to="/" slot="left">  
+            <mt-button icon="back" @click="handleClose">返回</mt-button>  
+          </router-link>  
+          <mt-button icon="more" slot="right" @click="more"></mt-button>  
+        </mt-header>  
+  </div>
+</template>
+
+<script>
+export default {
+  name:'heades',
+    methods: { 
+  	handleClose: function(){  
+        console.log("返回")  
+    },  
+    more: function(){  
+        console.log("更多")  
+    }  
+  }
+}
+</script>
+
+<style lang="scss" scoped="" type="text/css">
+  .demo-grid{
+    height: 0.533rem;
+    line-height: 0.533rem;
+    font-size:0.213rem;
+    color: #fff;
+    .searchBox{
+      position: relative;
+      i{
+        font-size: 0.213rem;
+        position: absolute;
+        top: 0.01rem;
+        left: 0.106rem;
+      }
+      input{
+        height: 0.4rem;
+        line-height: 0.4rem;
+        width: 100%;
+        border: none;
+        outline: none;
+        background: $backgroundSmall;
+        padding-left: 0.4rem;
+      }
+      .searchInput::-webkit-input-placeholder {
+            color: #fff;
+         }
+    }
+    .navbar{
+      .icon-xiangxia{
+        font-size:0.15rem;
+        margin-left:0.066rem;
+        vertical-align: middle;
+      }
+    }
+  }
+</style>
---
+++ src/components/Swife.vue
@@ -0,0 +1,39 @@
+<template>
+ <p>
+ <mt-swipe :auto="2000">
+  <mt-swipe-item v-for="item in items" :key="item.id">
+  <a :href="item.href" rel="external nofollow" >
+   <img :src="item.url" class="img"/>
+   <span class="desc"></span>
+  </a>
+  </mt-swipe-item>
+ </mt-swipe>
+ </p>
+</template>
+<script>
+ export default {
+ data () {
+  return {
+  items: [{
+   title: '你的名字',
+   href: 'http://google.com',   url: 'http://localhost:8080/static/img1.png'
+  }, {
+   title: '我的名字',
+   href: 'http://baidu.com',   url: 'http://localhost:8080/static/img2.png'
+  }]
+  }
+ }
+ }
+</script>
+<style>
+	.imageBox{
+		height: 3.12rem;
+		.swiper-slide{
+			margin: 0;
+		}
+	}
+	img{
+		width: 100%;
+		height: 3.12rem;
+	}
+</style>
